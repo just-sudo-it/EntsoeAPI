0ccb0f844ff68d9a190c2b8e22d1397d9489570e
just-sudo-it <lefteris.michailides@gmail.com>
2022-02-24 00:36:31 +0100

    Integrate http client factory and performant logging in background service

--- src/StromPriserWidgetAPI.WebAPI/Services/UpdateDbService.cs
+++ src/StromPriserWidgetAPI.WebAPI/Services/UpdateDbService.cs
@@ -2,62 +2,83 @@ namespace StromPriserWidgetAPI.WebAPI.Services;
 
 using System.Diagnostics.CodeAnalysis;
 
-using Kritikos.StromPriserWidgetAPI.Data;
+using Microsoft.Net.Http.Headers;
+
+using StromPriserWidgetAPI.Data;
 
 public class UpdateDbService : BackgroundService
 {
-  protected readonly ILogger<UpdateDbService> _logger;
-  protected readonly IServiceScopeFactory _scopeFactory;
+  private readonly ILogger<UpdateDbService> logger;
+  private readonly IServiceScopeFactory scopeFactory;
+  private readonly IHttpClientFactory httpFactory;
 
   public UpdateDbService(
     ILogger<UpdateDbService> logger,
+    IHttpClientFactory httpFactory,
     [NotNull] IServiceScopeFactory scopeFactory)
   {
-    _logger = logger;
-    _scopeFactory = scopeFactory;
+    this.logger = logger;
+    this.httpFactory = httpFactory;
+    this.scopeFactory = scopeFactory;
   }
 
   public override Task StopAsync(CancellationToken cancellationToken)
   {
-    _logger.LogInformation("Timed Hosted Service is stopping.");
+    Log.ServiceStopped(logger, nameof(UpdateDbService));
 
     return Task.CompletedTask;
   }
 
+  [SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "Background service should not die")]
   protected override async Task ExecuteAsync(CancellationToken stoppingToken)
   {
-    _logger.LogInformation("Timed Hosted Service is running.");
-
-    while (!stoppingToken.IsCancellationRequested)
+    try
     {
-      await UpdateDatabase(stoppingToken);
+      Log.ServiceStarted(logger, nameof(UpdateDbService));
 
-      await Task.Delay(TimeSpan.FromHours(24), stoppingToken);
+      while (!stoppingToken.IsCancellationRequested)
+      {
+        await UpdateDatabase(stoppingToken);
+
+        await Task.Delay(TimeSpan.FromHours(24), stoppingToken);
+      }
+    }
+    catch (Exception ex)
+    {
+      Log.ServiceFailed(logger, nameof(UpdateDbService), ex);
     }
   }
 
   private Task UpdateDatabase(CancellationToken cancellationToken)
   {
-    using (var scope = _scopeFactory.CreateScope())
+    using (var scope = scopeFactory.CreateScope())
     {
       using (var dbContext = scope.ServiceProvider.GetRequiredService<DataContext>())
       {
-        try
-        {
-
-
-        }
-        catch (Exception e)
-        {
-          _logger.LogError(e.Message);
-        }
       }
     }
+
     return Task.CompletedTask;
   }
 
-  private Task GetPriceData(CancellationToken cancellationToken)
+  private async Task GetPriceData(CancellationToken cancellationToken)
   {
-    return Task.CompletedTask;
+    using var client = httpFactory.CreateClient();
+    using var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, "https://api.github.com/repos/dotnet/AspNetCore.Docs/branches")
+    {
+      Headers = { { HeaderNames.Accept, "application/vnd.github.v3+json" }, { HeaderNames.UserAgent, "HttpRequestsSample" }, },
+    };
+    var httpResponseMessage = await client.SendAsync(httpRequestMessage, cancellationToken);
+
+    if (httpResponseMessage.IsSuccessStatusCode)
+    {
+      using var contentStream = await httpResponseMessage.Content.ReadAsStreamAsync(cancellationToken);
+
+      /*     GitHubBranches = await JsonSerializer.DeserializeAsync
+               <IEnumerable<GitHubBranch>>(contentStream);
+        */
+    }
+
+    return;
   }
 }
