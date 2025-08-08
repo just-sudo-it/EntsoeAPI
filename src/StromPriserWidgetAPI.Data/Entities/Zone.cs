c8d06c3c1c3f3fb1b8eb2f4bcf456e5ccdb8ed77
just-sudo-it <57672397+just-sudo-it@users.noreply.github.com>
2025-08-08 03:26:13 +0200

    Refine zone price entity mapping

--- src/StromPriserWidgetAPI.Data/Entities/Zone.cs
+++ src/StromPriserWidgetAPI.Data/Entities/Zone.cs
@@ -1,5 +1,17 @@
 namespace StromPriserWidgetAPI.Data.Entities;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 public class Zone : StromPriserEntity<long, Zone>
 {
   public string Name { get; set; } = string.Empty;
+
+  internal static void OnModelCreating(ModelBuilder builder)
+  {
+    EntityTypeBuilder<Zone> e = builder.Entity<Zone>();
+    StromPriserEntity<long, Zone>.OnModelCreating(e);
+
+    e.Property(z => z.Name).IsRequired();
+  }
 }
--- src/StromPriserWidgetAPI.Data/Entities/ZonePrice.cs
+++ src/StromPriserWidgetAPI.Data/Entities/ZonePrice.cs
@@ -1,9 +1,21 @@
 namespace StromPriserWidgetAPI.Data.Entities;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 public class ZonePrice : StromPriserEntity<long, ZonePrice>
 {
-  public Zone Zone { get; set; } = new Zone();
+  public Zone Zone { get; set; } = null!;
 
   // NOK/kW
   public float Price { get; set; }
   public DateTime Date { get; set; }
+
+  internal static void OnModelCreating(ModelBuilder builder)
+  {
+    EntityTypeBuilder<ZonePrice> e = builder.Entity<ZonePrice>();
+    StromPriserEntity<long, ZonePrice>.OnModelCreating(e);
+
+    e.HasOne(zp => zp.Zone).WithMany().IsRequired();
+  }
 }
